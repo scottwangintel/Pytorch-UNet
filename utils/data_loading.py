c6fc990a1867f3cbd03f4553d4d056ec9f4d1e43
happy-gpu3 <scott.wang@intel.com>
2024-07-06 18:05:49 +0800

    enable endo data loading and need to check the datasets again

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -115,3 +115,37 @@ class BasicDataset(Dataset):
 class CarvanaDataset(BasicDataset):
     def __init__(self, images_dir, mask_dir, scale=1):
         super().__init__(images_dir, mask_dir, scale, mask_suffix='_mask')
+
+class EndoDataset(BasicDataset):
+    def __init__(self, images_dir, mask_dir, scale=1):
+        super().__init__(images_dir, mask_dir, scale, mask_suffix='')
+  
+    @staticmethod
+    def preprocess(mask_values, pil_img, scale, is_mask):
+        w, h = pil_img.size
+        newW, newH = int(scale * w), int(scale * h)
+        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
+        pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST if is_mask else Image.BICUBIC)
+        img = np.asarray(pil_img)
+
+        if is_mask:
+            mask = np.zeros((newH, newW), dtype=np.int64)
+            for i, v in enumerate(mask_values):
+                
+                if img.ndim == 2:
+                    mask[img == v] = int(v > 128) # For nanonet mask, assume only two values 0 and 1
+                else:
+                    mask[(img == v).all(-1)] = int(v[0] > 128) # # For nanonet mask, assume only two values 0 and 1
+
+            return mask
+
+        else:
+            if img.ndim == 2:
+                img = img[np.newaxis, ...]
+            else:
+                img = img.transpose((2, 0, 1))
+
+            if (img > 1).any():
+                img = img / 255.0
+
+            return img
